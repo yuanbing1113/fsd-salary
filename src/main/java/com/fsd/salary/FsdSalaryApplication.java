9b441a7b51c607eb9d7c1f235e26440b01a220a9
yuanbing1113 <yuanbing1113@gmail.com>
2019-07-27 20:27:23 +0800

    first commit

---
+++ src/main/java/com/fsd/salary/FsdSalaryApplication.java
@@ -0,0 +1,13 @@
+package com.fsd.salary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsdSalaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsdSalaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fsd/salary/FsdSalaryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fsd.salary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FsdSalaryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
