da2659d83e5df1ca79842c683cdc68323f470c82
yuanbing1113 <yuanbing1113@gmail.com>
2019-07-27 20:38:16 +0800

    add employee model

---
+++ src/main/java/com/fsd/salary/model/Employee.java
@@ -0,0 +1,24 @@
+package com.fsd.salary.model;
+
+public class Employee {
+
+	private String name;
+	private Double salary;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+}
